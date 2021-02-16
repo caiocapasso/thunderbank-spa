2e3f50c63105d71edcd41662a4526c2c0974c1a6
igor.shimauti <igor.shimauti@linx.com.br>
2021-02-16 17:48:14 -0300

    Ajustes de layout

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,18 +1,24 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { AuthService } from "../../services/auth.service";
+import { Usuario } from "src/app/shared/models/usuario.model";
 
 @Component({
 	selector: "app-header",
 	templateUrl: "./header.component.html",
 	styleUrls: ["./header.component.scss"]
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 	constructor(private router: Router, private authService: AuthService) {}
 
 	logout(): void {
 		console.log("user logged out");
 		this.authService.logout();
 		this.router.navigate(["home"]);
+	}
+
+	usuario: any;
+	ngOnInit(): void {
+		this.usuario = this.authService.getUser();
 	}
 }
